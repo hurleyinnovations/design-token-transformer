bd55d6a275e38f475f95db2e4e0fce6c0968edac
Automated Publisher <actions@users.noreply.github.com>
2023-08-31 15:01:41 +0000

    Automated publish: Thu Aug 31 15:01:41 UTC 2023 a8e5640c16276faf91e54d9b11c0bc0331edb2cb

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -4,18 +4,17 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 31 Aug 2023 14:41:49 GMT
+// Generated on Thu, 31 Aug 2023 15:01:41 GMT
 
 
 import UIKit
 
 public class StyleDictionary {
-    public static let colorColorsEmpty = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
-    public static let colorColorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorColorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorColorsRefBlue = [object Object]
-    public static let colorColorsSemiRed = UIColor(red: 1.000, green: 0.373, blue: 0.016, alpha: 0.5019607843137255)
-    public static let colorColorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorColorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorColorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let fontBodyExtraBoldCondensedItalic = "[object Object]"
+    public static let fontBodyH3 = "[object Object]"
+    public static let fontBodyH4StrikeThrough = "[object Object]"
+    public static let fontBodyItalic = "[object Object]"
+    public static let fontBodyMediumExtendedItalic = "[object Object]"
+    public static let fontBodySuper = "[object Object]"
+    public static let fontBodyVariableFont = "[object Object]"
 }
